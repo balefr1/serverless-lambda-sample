6b3896657edb0fadbd81680b0c7b2bb55a6e8232
Sereno Balestra <sbalestra@sorint.com>
2024-04-07 23:16:19 +0200

    fix

--- sed_sync.sh
+++ sed_sync.sh
@@ -1,3 +1,8 @@
-sed -ie "s/gqmdi3ux67.execute-api.eu-south-1.amazonaws.com/$1/g" frontend/react/build/static/js/*
+if [[ $# -eq 0 ]] ; then
+    echo 'required params: apigw_domain static_bucket_name'
+    exit 1
+fi
+
+sed -ie "s/api.example.com/$1/g" frontend/react/build/static/js/*
 cd frontend/react/build
 aws s3 sync . s3://$2
